0d9f879f6a66c56f6c714c16d14b43e1d8bf40d3
khanhdoth <khanh.doth@gmail.com>
2020-04-15 09:34:43 +0000

    update

--- install.sh
+++ install.sh
@@ -3,3 +3,11 @@
 # export DEBIAN_FRONTEND=noninteractive
 apt update
 apt -y install docker.io
+cd /home/khanhdoth
+mkdir dev
+cd dev
+mkdir git
+cd git
+git clone https://github.com/khanhdoth/my_hello
+cd my_hello
+sh dev.build.sh
