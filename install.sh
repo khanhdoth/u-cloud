b695c91e4d49a69442f0192347bc80164045fb16
khanhdoth <khanh.doth@gmail.com>
2020-04-15 10:51:45 +0000

    update

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ apt update
 apt -y install docker.io
 
 # Build my_hello dev container
-cd /home/khanhdoth
+cd /home/khanh_doth
 mkdir dev
 
 cd dev
@@ -22,15 +22,15 @@ cd my_hello
 sh dev.build.sh
 
 # Build Jenkins container
-cd /home/khanhdoth
+cd /home/khanh_doth
 docker run -u 0 -dit --name my-running-jenkins -e JENKINS_OPTS="--prefix=/jenkins" -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home -v "$PWD:/home/host" -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker jenkins/jenkins:lts
 
 # Build CodeServer container
-cd /home/khanhdoth
+cd /home/khanh_doth
 docker run -u 0 -dit --name my-code-server -p 8081:8080 -v "$PWD:/home/coder/project" -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker codercom/code-server --auth none 
 
 # Build Portainer container
-cd /home/khanhdoth
+cd /home/khanh_doth
 docker volume create portainer_data
 docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
 
