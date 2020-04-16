65c0228db9edf7a6a97127a69b4ec7169b1f82ad
khanhdoth <khanh.doth@gmail.com>
2020-04-16 23:25:56 +0200

    update

--- install.sh
+++ install.sh
@@ -50,6 +50,14 @@ docker run -u 0 -dit --name my-running-jenkins -e JENKINS_OPTS="--prefix=/jenkin
 ufw allow 8080
 ufw allow 50000
 
+# Build CodeServer container
+cd /home/khanh_doth
+docker run -u 0 -dit --name my-code-server -p 8081:8080 -v "$PWD:/home/coder/project" -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker codercom/code-server --auth none 
+ufw allow 8081
+docker exec -dit my-code-server git config --global user.email "khanh.doth@gmail.com"
+docker exec -dit my-code-server git config --global user.name "khanhdoth"
+docker exec -dit my-code-server git config --global credential.helper store
+
 # Build Portainer container
 cd /home/khanh_doth
 docker volume create portainer_data
@@ -57,10 +65,3 @@ docker run -dit -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -
 ufw allow 8000
 ufw allow 9000
 
-# Build CodeServer container
-cd /home/khanh_doth
-docker run -u 0 -dit --name my-code-server -p 8081:8080 -v "$PWD:/home/coder/project" -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker codercom/code-server --auth none 
-ufw allow 8081
-docker exec my-code-server git config --global user.email "khanh.doth@gmail.com"
-docker exec my-code-server git config --global user.name "khanhdoth"
-docker exec my-code-server git config --global credential.helper store
