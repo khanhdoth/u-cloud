88bc963574e2c67efc67beda48434fe2a9a47e9e
khanhdoth <khanh.doth@gmail.com>
2020-04-19 12:13:46 +0200

    update

--- install.sh
+++ install.sh
@@ -2,17 +2,6 @@
 
 # export DEBIAN_FRONTEND=noninteractive
 
-# show IPs
-echo
-ifconfig
-
-# read localhost IP
-echo 
-echo Please enter the localhost IP which the clients will connect to?
-echo
-# read myLocalhostIP
-# echo The IP $myLocalhostIP is entered and will be used for configuration. 
-
 # update
 apt update
 
@@ -49,18 +38,16 @@ cd /home/khanh_doth
 docker run -u 0 -dit --name my-running-jenkins -e JENKINS_OPTS="--prefix=/jenkins" -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home -v "$PWD:/home/host" -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker jenkins/jenkins:lts
 ufw allow 8080
 ufw allow 50000
-
+# Build CodeServer container
+cd /home/khanh_doth
+docker run -u 0 -dit --name my-code-server -p 8081:8080 -v "$PWD:/home/coder/project" -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker codercom/code-server --auth none 
+ufw allow 8081
 # Build Portainer container
 cd /home/khanh_doth
 docker volume create portainer_data
-docker run -dit -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer:latest
+docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
 ufw allow 8000
 ufw allow 9000
-
-# Build CodeServer container
-cd /home/khanh_doth
-docker run -u 0 -dit --name my-code-server -p 8081:8080 -v "$PWD:/home/coder/project" -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker codercom/code-server:latest --auth none 
-ufw allow 8081
 
 # docker exec -dit my-code-server git config --global user.email "khanh.doth@gmail.com"
 # docker exec -dit my-code-server git config --global user.name "khanhdoth"
