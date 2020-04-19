cc5790ee943fd6b00e075730dbc6303b883a0e89
khanhdoth <khanh.doth@gmail.com>
2020-04-19 19:21:10 +0000

    u

--- postgres/build.sh
+++ postgres/build.sh
@@ -4,6 +4,17 @@ echo "Build postgres container"
 # delete running container
 docker rm -f my-running-postgres
 
+# create required folder 
+mkdir /home/khanh_doth/dev/git/u-cloud/postgres/data/pg_commit_ts
+mkdir /home/khanh_doth/dev/git/u-cloud/postgres/data/pg_dynshmem
+mkdir /home/khanh_doth/dev/git/u-cloud/postgres/data/pg_replslot
+mkdir /home/khanh_doth/dev/git/u-cloud/postgres/data/pg_serial
+mkdir /home/khanh_doth/dev/git/u-cloud/postgres/data/pg_snapshots
+mkdir /home/khanh_doth/dev/git/u-cloud/postgres/data/pg_stat
+mkdir /home/khanh_doth/dev/git/u-cloud/postgres/data/pg_stat_tmp
+mkdir /home/khanh_doth/dev/git/u-cloud/postgres/data/pg_tblspc
+mkdir /home/khanh_doth/dev/git/u-cloud/postgres/data/pg_twophase
+
 # create new container 
 docker run -u 0 -dit --name my-running-postgres -v /home/khanh_doth/dev/git/u-cloud/postgres/data:/var/lib/postgresql/data -p 5432:5432 -e POSTGRES_PASSWORD=postgres -d postgres
 
