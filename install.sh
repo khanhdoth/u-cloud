f3cbec247335f0dd0a60c1fa4f21949b863f2bba
khanhdoth <khanh.doth@gmail.com>
2020-04-19 15:20:57 +0000

    update

--- install.sh
+++ install.sh
@@ -36,9 +36,4 @@ sh /home/khanh_doth/dev/git/u-cloud/jenkins/build.sh
 sh /home/khanh_doth/dev/git/u-cloud/codeserver/build.sh
 
 # Build Portainer container
-cd /home/khanh_doth
-docker volume create portainer_data
-docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
-ufw allow 8000
-ufw allow 9000
-
+sh /home/khanh_doth/dev/git/u-cloud/portainer/build.sh
