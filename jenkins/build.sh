b402b5716c9ee5e5df8086675edf4987e2929dca
khanhdoth <khanh.doth@gmail.com>
2020-04-19 14:51:21 +0000

    update

---
+++ jenkins/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Build Jenkins container"
+
+echo "Current user is :"
+whoami
+
+cd /home/khanh_doth/dev/git/u-cloud/jenkins
+
+# delete running container
+docker rm -f my-running-jenkins
+
+# create new container
+cd /home/khanh_doth
+# docker run -u 0 -dit --name my-running-jenkins -e JENKINS_OPTS="--prefix=/jenkins" -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home -v "$PWD:/home/host" -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker jenkins/jenkins:lts
+docker run -u 0 -dit --name my-running-jenkins -e JENKINS_OPTS="--prefix=/jenkins" -p 8080:8080 -p 50000:50000 -v /home/khanh_doth/dev/git/u-cloud/postgres/jenkins_home:/var/jenkins_home -v "$PWD:/home/host" -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker jenkins/jenkins:lts
