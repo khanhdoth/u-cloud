51a41c9ae18539c9f36a1bc36d9a6c683fb127ed
khanhdoth <khanh.doth@gmail.com>
2020-04-19 17:42:48 +0000

    update

--- postgres/build.sh
+++ postgres/build.sh
@@ -5,6 +5,6 @@ echo "Build postgres container"
 docker rm -f my-running-postgres
 
 # create new container 
-docker run -dit --name my-running-postgres -v /home/khanh_doth/dev/git/u-cloud/postgres/data:/var/lib/postgresql/data -p 5432:5432 -e POSTGRES_PASSWORD=postgres -d postgres
+docker run -u 0 -dit --name my-running-postgres -v /home/khanh_doth/dev/git/u-cloud/postgres/data:/var/lib/postgresql/data -p 5432:5432 -e POSTGRES_PASSWORD=postgres -d postgres
 
 ufw allow 5432
