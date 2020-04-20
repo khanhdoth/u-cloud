e4a33f8b377ac57c6c3e239e7604a1f07a993714
khanhdoth <khanh.doth@gmail.com>
2020-04-20 15:00:25 +0000

    u

--- postgres/build.sh
+++ postgres/build.sh
@@ -8,6 +8,6 @@ docker rm -f my-running-postgres
 sh /home/khanh_doth/dev/git/u-cloud/postgres/database.restore.sh
 
 # create new container 
-docker run -u 0 -dit --name my-running-postgres -v /home/khanh_doth/dbdata:/var/lib/postgresql/data -p 5432:5432 -e POSTGRES_PASSWORD=postgres -d postgres
+docker run -u 0 -it --name my-running-postgres -v /home/khanh_doth/dbdata:/var/lib/postgresql/data -p 5432:5432 -e POSTGRES_PASSWORD=postgres -d postgres
 
 ufw allow 5432
