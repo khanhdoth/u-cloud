85efd6417bda49cab78986b74657c1a1d2b9bd14
khanhdoth <khanh.doth@gmail.com>
2020-04-24 14:11:34 +0000

    u

--- apache/build.sh
+++ apache/build.sh
@@ -4,6 +4,7 @@ echo "Build Apache HTTDP container"
 echo "Current user is :"
 whoami
 
+cp -r /home/khanh_doth/dev/git/u-cloud/cert /home/khanh_doth/dev/git/u-cloud/apache/cert
 cd /home/khanh_doth/dev/git/u-cloud/apache
 
 # delete running container
@@ -14,6 +15,8 @@ docker image rm my-apache
 
 # build new image
 docker build -t my-apache .
+
+rm -r /home/khanh_doth/dev/git/u-cloud/apache/cert
 
 # create new container based on new image
 cd /home/khanh_doth
