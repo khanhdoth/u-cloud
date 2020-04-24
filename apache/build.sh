a4ea8e730a60f5b7fdcf1801e4ab51ab6a50d607
khanhdoth <khanh.doth@gmail.com>
2020-04-24 13:37:20 +0000

    u

---
+++ apache/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Build Apache HTTDP container"
+
+echo "Current user is :"
+whoami
+
+cd /home/khanh_doth/dev/git/u-cloud/apache
+
+# delete running container
+docker rm -f my-running-apache
+
+# delete image
+docker image rm my-apache
+
+# build new image
+docker build -t my-apache .
+
+# create new container based on new image
+cd /home/khanh_doth
+docker run -dit --name my-running-apache -p 80:80 -p 443:443 -v "$PWD":/usr/local/apache2/htdocs/ my-apache
+
+ufw allow 80
+ufw allow 443
