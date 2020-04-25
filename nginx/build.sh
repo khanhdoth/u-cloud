be3c836e16eae1b017112e34d2326cf61727437a
khanhdoth <khanh.doth@gmail.com>
2020-04-25 08:29:57 +0000

    u

---
+++ nginx/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "Build Nginx container"
+
+echo "Current user is :"
+whoami
+
+cp -r /home/khanh_doth/dev/git/u-cloud/cert /home/khanh_doth/dev/git/u-cloud/nginx/cert
+cd /home/khanh_doth/dev/git/u-cloud/nginx
+
+# delete running container
+docker rm -f my-running-nginx
+
+# delete image
+docker image rm my-nginx
+
+# build new image
+docker build -t my-nginx .
+
+rm -r /home/khanh_doth/dev/git/u-cloud/nginx/cert
+
+# create new container based on new image
+cd /home/khanh_doth
+docker run -dit --name my-running-nginx -p 80:80 -p 443:443 -v "$PWD":/usr/share/nginx/html:ro my-nginx
+
+ufw allow 80
+ufw allow 443
