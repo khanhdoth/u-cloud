cf98116dd17e29079356231a047ab92da2f8071e
khanhdoth <khanh.doth@gmail.com>
2020-04-26 17:31:39 +0000

    u

--- nginx/copy_certificate_from_container.sh
+++ nginx/copy_certificate_from_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -r /home/khanh_doth/dev/git/u-cloud/nginx/letsencrypt
-docker cp my-running-nginx:/etc/letsencrypt/live/. /home/khanh_doth/dev/git/u-cloud/nginx/letsencrypt
+docker cp my-running-nginx:/etc/letsencrypt/archive/. /home/khanh_doth/dev/git/u-cloud/nginx/letsencrypt
