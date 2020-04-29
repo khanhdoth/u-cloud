3ff773501eaffffad5b0f6cba3a6a4f22215692c
khanhdoth <khanh.doth@gmail.com>
2020-04-29 15:20:39 +0000

    u

---
+++ codeserver/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# delete running container
+docker rm -f my-running-code-server
+
+# create new container based on new image
+cd /home/khanh_doth
+docker run -u 0 -dit --name my-running-code-server -p 8081:8080 -p 6000-7000:6000-7000 -v "$PWD:/home/coder/project" -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker my-code-server --auth none
