52d95bcb3eb401153f037396f6867efea335038a
khanhdoth <khanh.doth@gmail.com>
2020-05-08 10:07:56 +0000

    u

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -13,7 +13,7 @@ docker rm -f my-running-jenkins
 cd /home/khanh_doth
 # docker run -u 0 -dit --name my-running-jenkins -e JENKINS_OPTS="--prefix=/jenkins" -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home -v "$PWD:/home/host" -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker jenkins/jenkins:lts
 docker pull jenkins/jenkins:lts
-docker run -u 0 -dit --name my-running-jenkins -e JENKINS_OPTS="--prefix=/jenkins" -p 8080:8080 -p 50000:50000 -v /home/khanh_doth/dev/git/u-cloud/jenkins/jenkins_home:/var/jenkins_home -v "$PWD:/home/host" -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker jenkins/jenkins:lts
+docker run -u 0 -dit --restart always --name my-running-jenkins -e JENKINS_OPTS="--prefix=/jenkins" -p 8080:8080 -p 50000:50000 -v /home/khanh_doth/dev/git/u-cloud/jenkins/jenkins_home:/var/jenkins_home -v "$PWD:/home/host" -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker jenkins/jenkins:lts
 
 ufw allow 8080
 ufw allow 50000
--- nginx/run.sh
+++ nginx/run.sh
@@ -5,7 +5,7 @@ echo "Run Nginx container"
 docker rm -f my-running-nginx
 
 cd /home/khanh_doth
-docker run -dit --name my-running-nginx -p 80:80 -p 443:443 \
+docker run -dit --restart always --name my-running-nginx -p 80:80 -p 443:443 \
     -v /home/khanh_doth/dev/git/u-cloud/nginx/default.conf:/etc/nginx/conf.d/default.conf \
     -v /home/khanh_doth/dev/git/u-cloud/nginx/nginx.conf:/etc/nginx/nginx.conf \
     -v "$PWD":/usr/share/nginx/html:ro \
