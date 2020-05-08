adb147d492d24420445cc303f6db5d6b5c1b7be7
khanhdoth <khanh.doth@gmail.com>
2020-05-08 19:28:07 +0000

    u

---
+++ codeserver/build-3.3.0-rc.7.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Build CodeServer container"
+
+echo "Current user is :"
+whoami
+
+cd /home/khanh_doth/dev/git/u-cloud/codeserver
+
+# delete running container
+docker rm -f my-running-code-server
+
+# delete image
+docker image rm khanhdo/my-code-server
+
+# build new image
+docker build -t khanhdo/my-code-server . -f Dockerfile-3.3.0-rc.7
+
+# create new container based on new image
+sh run.sh
+
+ufw allow 6001
+ufw allow 6501
+ufw allow 8081
