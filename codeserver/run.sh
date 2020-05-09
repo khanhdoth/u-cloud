2832094b7d01169aef720ebc06b24b5508190c8e
khanhdoth <khanh.doth@gmail.com>
2020-05-09 15:22:57 +0000

    u

--- codeserver/run.sh
+++ codeserver/run.sh
@@ -5,4 +5,4 @@ docker rm -f my-running-code-server
 
 # create new container based on new image
 cd /home/khanh_doth
-docker run -u 0 -dit --restart always --name my-running-code-server -p 8081:8080 -p 4000-5000:4000-5000 -p 6000-7000:6000-7000 -v "$PWD:/home/coder/project" -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker khanhdo/my-code-server --auth none
+docker run -u 0 -dit --restart always --name my-running-code-server -p 8081:8080 -p 4000-5000:4000-5000 -p 6000-7000:6000-7000 -v "$PWD:/home/coder/project" -v /var/run/docker.sock:/var/run/docker.sock -v "/tmp/cloudsql/dev-sulof-com:us-central1:hellodb":"/tmp/cloudsql/dev-sulof-com:us-central1:hellodb" -v /usr/bin/docker:/usr/bin/docker khanhdo/my-code-server --auth none
