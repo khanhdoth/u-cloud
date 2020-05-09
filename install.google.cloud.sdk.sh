f4d7370de17f958c91bce7b5a8a70f18961efbfc
khanhdoth <khanh.doth@gmail.com>
2020-05-09 12:28:10 +0000

    u

--- install.google.cloud.sdk.sh
+++ install.google.cloud.sdk.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+GOOGLE_SDK="google-cloud-sdk-291.0.0-linux-x86_64.tar.gz"
 
 # create dev/git and clone projects from git
 cd /home/khanh_doth/dev
@@ -6,6 +7,8 @@ cd /home/khanh_doth/dev
 rm -r cloud-sdk
 mkdir cloud-sdk
 cd cloud-sdk
-
-wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-291.0.0-linux-x86_64.tar.gz
-tar -xvzf google-cloud-sdk-291.0.0-linux-x86_64.tar.gz
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/$GOOGLE_SDK
+tar -xvzf $GOOGLE_SDK
+rm $GOOGLE_SDK
+yes Y | sh google-cloud-sdk/install.sh
+./google-cloud-sdk/bin/gcloud init
