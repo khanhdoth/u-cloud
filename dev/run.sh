cdf2af3f5d6aed5933bc07d2e2828405885fe2bb
khanhdoth <khanh.doth@gmail.com>
2020-05-10 18:59:34 +0000

    u

--- dev/run.sh
+++ dev/run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # delete running container
-docker rm -f my-dev
+docker rm -f my-running-dev
 
 # create new container based on new image
 cd /home/khanh_doth
 # PATHA="/tmp/cloudsql/dev-sulof-com:us-central1:hellodb"
 # docker run -u 0 -dit --restart always --name my-dev --mount type=bind,source="$PATHA",destination="$PATHA" \
-docker run -u 0 -dit --restart always --name my-dev \
+docker run -u 0 -dit --restart always --name my-running-dev --workdir /home/dev/git \
     -p 4000-5000:4000-5000 \
     -p 6000-7000:6000-7000 \
+    -v "$PWD:/home" \
     khanhdo/my-dev
