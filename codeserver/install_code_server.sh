aa3091b7c908570904dd9d7de146fe27acbf5a41
khanhdoth <khanh.doth@gmail.com>
2020-05-17 09:19:12 +0000

    u

---
+++ codeserver/install_code_server.sh
@@ -0,0 +1,11 @@
+CS_VERSION=code-server-3.2.0-linux-arm64.tar.gz
+useradd -m -s /bin/bash code
+passwd code
+su - code
+wget https://github.com/cdr/code-server/releases/download/3.2.0/$CS_VERSION
+tar -xf $CS_VERSION
+mv $CS_VERSION/ bin/
+chmod +x ~/bin/code-server
+mkdir -p ~/data
+cd /etc/systemd/system/
+
