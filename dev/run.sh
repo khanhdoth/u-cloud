a1b830e4cdba950b9b90be766453caba7f86105a
khanhdoth <khanh.doth@gmail.com>
2020-05-20 21:03:09 +0200

    u

--- dev/run.sh
+++ dev/run.sh
@@ -8,7 +8,7 @@ cd /home/khanh_doth
 # PATHA="/tmp/cloudsql/dev-sulof-com:us-central1:hellodb"
 # docker run -u 0 -dit --restart always --name my-dev --mount type=bind,source="$PATHA",destination="$PATHA" \
 docker run -u 0 -dit --restart always --name my-running-dev --workdir /home/dev/git \
-    -p 4000-5000:4000-5000 \
-    -p 6000-7000:6000-7000 \
+    -p 4000-4010:4000-4010 \
+    -p 6000-6010:6000-6010 \
     -v "$PWD:/home" \
     khanhdo/my-dev
